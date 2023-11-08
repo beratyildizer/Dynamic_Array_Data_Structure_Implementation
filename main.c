a8cb412834763d144dc4f9d72ef40035aba7a792
Berat Yıldızer <44676948+beratyildizer@users.noreply.github.com>
2023-11-08 15:42:33 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,93 @@
+// dynamicArrayDataStructureImplementation.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "dynamicArray.h"
+
+int main()
+{
+    dynamicArray* p = createDynamicArray(20);
+    pushBack(p, 10);
+    pushBack(p, 11);
+    pushBack(p, 12);
+    pushBack(p, 10);
+    pushBack(p, 10);
+    pushBack(p, 10);
+    pushBack(p, 14);
+    pushBack(p, 15);
+    pushBack(p, 15);
+    printDynamicArray(p);
+    pushBack(p, 15);
+    printDynamicArray(p);
+    pushBack(p, 15);
+    printDynamicArray(p);
+    pushBack(p, 15);
+    pushBack(p, 15);
+    printDynamicArray(p); pushBack(p, 15);
+    printDynamicArray(p); pushBack(p, 15);
+    printDynamicArray(p); pushBack(p, 15);
+    printDynamicArray(p); pushBack(p, 15);
+    printDynamicArray(p); pushBack(p, 15);
+    printDynamicArray(p); pushBack(p, 15);
+    printDynamicArray(p); pushBack(p, 15);
+    printDynamicArray(p); pushBack(p, 15);
+    printDynamicArray(p); pushBack(p, 15);
+    printDynamicArray(p); pushBack(p, 15);
+    printDynamicArray(p); pushBack(p, 15);
+    printDynamicArray(p); pushBack(p, 15);
+    printDynamicArray(p); pushBack(p, 15);
+    printDynamicArray(p); pushBack(p, 15);
+    printDynamicArray(p); pushBack(p, 15);
+    printDynamicArray(p); pushBack(p, 15);
+    printDynamicArray(p); pushBack(p, 15);
+    printDynamicArray(p); pushBack(p, 15);
+    printDynamicArray(p); pushBack(p, 15);
+    printDynamicArray(p); pushBack(p, 15);
+    printDynamicArray(p); pushBack(p, 15);
+    printDynamicArray(p); pushBack(p, 15);
+    printDynamicArray(p); pushBack(p, 15);
+    printDynamicArray(p); pushBack(p, 15);
+    printDynamicArray(p); pushBack(p, 15);
+    printDynamicArray(p); pushBack(p, 15);
+    printDynamicArray(p); pushBack(p, 15);
+    printDynamicArray(p); pushBack(p, 15);
+    printDynamicArray(p); pushBack(p, 15);
+    printDynamicArray(p); pushBack(p, 15);
+    printDynamicArray(p); pushBack(p, 15);
+    printDynamicArray(p); pushBack(p, 15);
+    printDynamicArray(p); pushBack(p, 15);
+    printDynamicArray(p); pushBack(p, 15);
+    printDynamicArray(p); pushBack(p, 15);
+    printDynamicArray(p); pushBack(p, 15);
+    printDynamicArray(p); pushBack(p, 15);
+    printDynamicArray(p); pushBack(p, 15);
+    printDynamicArray(p); pushBack(p, 15);
+    printDynamicArray(p); pushBack(p, 15);
+    printDynamicArray(p); pushBack(p, 15);
+    printDynamicArray(p); pushBack(p, 15);
+    printDynamicArray(p); pushBack(p, 15);
+    printDynamicArray(p); pushBack(p, 15);
+    printDynamicArray(p); pushBack(p, 15);
+    printDynamicArray(p); pushBack(p, 15);
+    printDynamicArray(p); pushBack(p, 15);
+    printDynamicArray(p); pushBack(p, 15);
+    printDynamicArray(p); pushBack(p, 15);
+    printDynamicArray(p); pushBack(p, 15);
+    printDynamicArray(p); pushBack(p, 15);
+    printDynamicArray(p); pushBack(p, 15);
+    printDynamicArray(p); pushBack(p, 15);
+    printDynamicArray(p);
+    popBack(p);
+    printDynamicArray(p);
+    pushData(p, 24, 1);
+    printDynamicArray(p);
+    popData(p, 1);
+    printDynamicArray(p);
+    shrinkToFit(p);
+    printDynamicArray(p);
+    clearDynamicArray(p);
+    printDynamicArray(p);
+    shrinkToFit(p);
+    printDynamicArray(p);
+}
+
+
